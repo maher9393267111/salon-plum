ce9df673a09107d5de8f284660d5c81a87882eff
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2023-07-02 12:13:46 +0300

    add seo

--- pages/_app.js
+++ pages/_app.js
@@ -43,7 +43,7 @@ function MyApp({ Component, pageProps ,props }) {
   return (
     <>
 
-<DefaultSeo
+{/* <DefaultSeo
                 title="nourabeautycenter96"
                 description="Nourabeauty center Välkommen till Noura beauty centers"
                 openGraph={{
@@ -58,7 +58,69 @@ function MyApp({ Component, pageProps ,props }) {
                     site: '@site',
                     cardType: 'summary_large_image',
                 }}
-            />
+            /> */}
+
+
+<Head>
+        <meta charset="utf-8" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#1192d3" />
+        <meta name="title" content="nourabeautycenter clinic" />
+        <meta
+          name="description"
+          content="nourabeautycenter clinic"
+        />
+        <meta
+          name="keywords"
+          content="nourabeautycenter,"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="author" content="nourabeautycenter" />
+        <meta property="og:locale" content="en-US" />
+
+        {/* <!-- Primary Meta Tags --> */}
+        <title>nourabeautycenter clinic </title>
+        <meta name="title" content="nourabeautycenter" />
+        <meta
+          name="description"
+          content="nourabeauty center "
+        />
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.nourabeautycenter.se" />
+        <meta property="og:title" content="nourabeautycenter clinic" />
+        <meta
+          property="og:description"
+          content="nourabeautycenter clinic"
+        />
+        <meta property="og:image" content="meta-tn.png" />
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://www.nourabeautycenter.se" />
+        <meta
+          property="twitter:title"
+          content="nourabeautycenter clinic"
+        />
+        <meta
+          property="twitter:description"
+          content="nourabeautycenter clinic"
+        />
+        <meta property="twitter:image" content="meta-tn.png" />
+
+        <meta
+          name="google-site-verification"
+          content="nstIYPUM8pyaUUrW69SvgmJkxRRe_hS9tN_VAfzoLeI"
+        />
+        <title>nourabeautycenter clinic</title>
+      </Head>
+
+
+
 
 
 
